e8e01e876f399a8dfa6c1f5521a441135143bd9d
Prateek Jain <prateekjdelhi2004@gmail.com>
2026-03-19 15:22:11 +0530

    code

--- karma/src/components/CKNavbar.jsx
+++ karma/src/components/CKNavbar.jsx
@@ -100,7 +100,7 @@ export default function CKNavbar({
             aria-label={intl.formatMessage({ id: "app.aria.home" })}
           >
             <img
-              src="/Logo.png"
+              src="/logo.png"
               alt={intl.formatMessage({ id: "app.title" })}
               className="ck-header-logo mt-4"
               draggable={false}
